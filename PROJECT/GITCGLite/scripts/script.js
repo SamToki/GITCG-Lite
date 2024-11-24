d629e38d69b27a5c3e3a482e97fa13b69395d0be
Sam Toki <SamToki@outlook.com>
2024-11-24 18:41:16 +0800

    Update script.js

--- PROJECT/GITCGLite/scripts/script.js
+++ PROJECT/GITCGLite/scripts/script.js
@@ -14,7 +14,7 @@
 	// Declare variables
 	"use strict";
 		// Unsaved
-		const CurrentVersion = 0.41;
+		const CurrentVersion = 0.42;
 		var Game0 = {
 			Options: {
 				TransparentOperationPanel: false
